512815e319923fc28ee9e1665a0b97144d2fe0ab
Hernan Rodriguez Valls <hernan.r.valls@gmail.com>
2022-05-24 18:08:55 +0200

    correccion del menu

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -8,10 +8,70 @@ class CustomAppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (_, BoxConstraints constraints) =>
+            (constraints.maxWidth > 520) ? _TableDesktopMenu() : _MobileMenu());
+  }
+}
+
+class _TableDesktopMenu extends StatelessWidget {
+  const _TableDesktopMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Row(
+        children: [
+          CustomFlatButton(
+            text: 'Contador Stateful',
+            color: Colors.black,
+            // onPressed: () {
+            //   Navigator.pushNamed(context, '/stateful');
+            // },
+            onPressed: () =>
+                locator<NavigationService>().navigateTo('/stateful'),
+          ),
+          const SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Contador con Provider',
+            color: Colors.black,
+            // onPressed: () {
+            //   Navigator.pushNamed(context, '/provider');
+            // },
+            onPressed: () =>
+                locator<NavigationService>().navigateTo('/provider'),
+          ),
+          const SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Otra Página',
+            color: Colors.black,
+            // onPressed: () {
+            //   Navigator.pushNamed(context, '/abc123');
+            // },
+            onPressed: () => locator<NavigationService>().navigateTo('/abc123'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileMenu extends StatelessWidget {
+  const _MobileMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
